1e1c7bd1879369ad95ce79b591b21c0b9f7c405d
MaryaLoskutova <s.mashustik.m@gmail.com>
2021-05-02 00:16:09 +0300

    Добавила ConsoleApp

---
+++ TextAnalyzer.ConsoleApp/App.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using TextAnalyzer.Service;
+using TextAnalyzer.Service.BusinessObjects;
+
+namespace TextAnalyzer.ConsoleApp
+{
+    public class App
+    {
+        private readonly IFileAnalyzingService _fileAnalyzingService;
+
+        public App(IFileAnalyzingService fileAnalyzingService)
+        {
+            _fileAnalyzingService = fileAnalyzingService;
+        }
+
+        public void Run(string[] args)
+        {
+            var path = GetPath();
+            var topCount = GetTopCount();
+
+            var words = _fileAnalyzingService.AnalyzeWords(path, topCount);
+
+            WriteResult(words);
+        }
+
+        private static void WriteResult(WordInfo[] words)
+        {
+            Console.WriteLine("\r\nРезультат");
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+
+        private static int GetTopCount()
+        {
+            Console.WriteLine("Введите количество слов (default 20):");
+            var countString = Console.ReadLine();
+            var topCount = string.IsNullOrEmpty(countString) ? 20 : int.Parse(countString);
+            return topCount;
+        }
+
+        private static string? GetPath()
+        {
+            Console.WriteLine("Введите путь к файлу (default \"voyna-i-mir.txt\"):");
+            var path = Console.ReadLine();
+            path = string.IsNullOrEmpty(path) ? Directory.GetCurrentDirectory() + "\\Example\\voyna-i-mir.txt" : path;
+            return path;
+        }
+    }
+}
---
+++ TextAnalyzer.ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TextAnalyzer.Service;
+
+namespace TextAnalyzer.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+            
+            var serviceProvider = services.BuildServiceProvider();
+            serviceProvider.GetService<App>().Run(args);
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IWordsAnalyzingService, WordsAnalyzingService>();
+            services.AddTransient<IFileAnalyzingService, FileAnalyzingService>();
+            services.AddTransient<App>();
+        }
+    }
+}
